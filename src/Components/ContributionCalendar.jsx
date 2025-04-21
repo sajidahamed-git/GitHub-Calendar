3fc417876fb454d4f5aaa537c77f9fe8ccf64ae3
Sajid <sajidahamed2001@gmail.com>
2025-04-22 02:16:31 +0530

    refactor: remove unused imports and state management from CalendarUI

--- src/Components/ContributionCalendar.jsx
+++ src/Components/ContributionCalendar.jsx
@@ -1,12 +1,5 @@
 // Imports
-import { useEffect, useState } from "react";
-import {
-  startOfYear,
-  endOfYear,
-  eachDayOfInterval,
-  getDay,
-  format,
-} from "date-fns";
+
 const years = [2021, 2022, 2023, 2024, 2025];
 // const weekdays = [1, 2, 3, 4, 5]; // Mon-Fri (1=Mon)
 const monthNames = [
@@ -32,59 +25,6 @@ const levelColors = [
 ];
 
 export default function CalendarUI() {
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const [weeks, setWeeks] = useState([]);
-  const [monthStarts, setMonthStarts] = useState([]);
-
-  //   const yearOptions = Array.from({ length: 5 }, (_, i) => 2022 + i);
-
-  useEffect(() => {
-    const start = startOfYear(new Date(selectedYear, 0, 1));
-    const end = endOfYear(start);
-
-    const days = eachDayOfInterval({ start, end });
-
-    // Group by weeks, filtering Mon-Fri
-    const calendarWeeks = [];
-    let currentWeek = [];
-
-    days.forEach((date) => {
-      const weekday = getDay(date); // 0 (Sun) to 6 (Sat)
-      const isoWeekday = weekday === 0 ? 7 : weekday; // Shift Sun to 7
-
-      if (isoWeekday >= 1 && isoWeekday <= 5) {
-        currentWeek.push({ date, level: Math.floor(Math.random() * 5) });
-      }
-
-      if (isoWeekday === 5) {
-        calendarWeeks.push(currentWeek);
-        currentWeek = [];
-      }
-    });
-
-    setWeeks(calendarWeeks.map((weekData, i) => ({ weekData, weekIndex: i })));
-
-    // Month starts: track first week index of each month
-    const seen = new Set();
-    const starts = [];
-
-    calendarWeeks.forEach((week, i) => {
-      for (let day of week) {
-        const month = day.date.getMonth();
-        if (!seen.has(month)) {
-          starts[month] = i;
-          seen.add(month);
-        }
-      }
-    });
-
-    setMonthStarts(starts);
-  }, [selectedYear]);
-
-  //   const handleYearChange = (e) => {
-  // setSelectedYear(Number(e.target.value));
-  //   };
-
   return (
     <div className=" flex flex-col bg-gray-950 text-gray-300 p-6 rounded-lg shadow-xl w-4/5  border border-gray-700">
       {/* Year Selector, first row */}
@@ -115,8 +55,10 @@ export default function CalendarUI() {
             <div>Sun</div>
           </div>
        
-            {monthNames.map((month, index) => (
-              <div className="">{month}</div>
+            {monthNames.map((month) => (
+              <div className="">{month}
+              {/*here i need a grid of cells*/}
+              </div>
             ))}
           </div>
      
